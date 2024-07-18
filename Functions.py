12ca68518604ba63ed33d39fb59e048e6392835d
lpiccinini25 <143562085+lpiccinini25@users.noreply.github.com>
2024-07-18 01:06:47 -0700

    finished puuid functuio

--- Functions.py
+++ Functions.py
@@ -2,12 +2,10 @@ import requests
 
 def get_puuid(name, riot_id):
     puuid_url = 'https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/' + name + '/' + riot_id
-    resp = 
-
-
-
-def get_final_url(api_url, api_key):
-    return api_url + '?api_key=' + api_key
+    resp = requests.get(puuid_url)
+    account_info = resp.json()
+    puuid = account_info['puuid']
+    return puuid
 
 def get_match_history(name, riot_id):
     'https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/' 
