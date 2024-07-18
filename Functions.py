d5ed8c25f3a2200c5381addac320c48e30bd20a7
lpiccinini25 <143562085+lpiccinini25@users.noreply.github.com>
2024-07-18 01:49:07 -0700

    fix puuid function

--- Functions.py
+++ Functions.py
@@ -9,3 +9,25 @@ def get_puuid(name, riot_id):
 
 def get_match_history(name, riot_id):
     'https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/' 
+
+def text_objects(text, font): #create text objects
+    textSurface = font.render(text, True, white)
+    return textSurface, textSurface.get_rect()
+
+
+def button(self,msg,x,y,w,h,ic,ac,action=None): #add button function
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(self.window, ac,(x,y,w,h))
+        if click[0] == 1 and action != None:
+            # runs whatever function is input, could be self.run() or quitgame()
+            print(action)
+            action()
+    else: # if no action, meaning button not clicked, then render buttons
+        pygame.draw.rect(self.window, ic,(x,y,w,h))
+        smallText = pygame.font.SysFont("comicsansms",20)
+        textSurf, textRect = text_objects(msg, smallText)
+        textRect.center = ( (x+(w/2)), (y+(h/2)) )
+        self.window.blit(textSurf, textRect)
--- RIOT_API_Project.py
+++ RIOT_API_Project.py
@@ -7,28 +7,6 @@ import functions
 black = (0,0,0)
 white = (255,255,255)
 
-def text_objects(text, font): #create text objects
-    textSurface = font.render(text, True, white)
-    return textSurface, textSurface.get_rect()
-
-
-def button(self,msg,x,y,w,h,ic,ac,action=None): #add button function
-    mouse = pygame.mouse.get_pos()
-    click = pygame.mouse.get_pressed()
-
-    if x+w > mouse[0] > x and y+h > mouse[1] > y:
-        pygame.draw.rect(self.window, ac,(x,y,w,h))
-        if click[0] == 1 and action != None:
-            # runs whatever function is input, could be self.run() or quitgame()
-            print(action)
-            action()
-    else: # if no action, meaning button not clicked, then render buttons
-        pygame.draw.rect(self.window, ic,(x,y,w,h))
-        smallText = pygame.font.SysFont("comicsansms",20)
-        textSurf, textRect = text_objects(msg, smallText)
-        textRect.center = ( (x+(w/2)), (y+(h/2)) )
-        self.window.blit(textSurf, textRect)
-
 api_key = 'RGAPI-267c12a7-85cd-4d85-9f5e-ea9d4d1645a3'
 api_url = 'https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/llimeincoconut/0000'
 
@@ -37,7 +15,7 @@ print(requests.get(api_url))
 
 api_url = api_url + '?api_key=' + api_key
 
-puuid = functions.get_puuid('llimeincoconut', '0000')
+puuid = functions.get_puuid('llimeincoconut', '0000', api_key)
 
 
 match_url = 'https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/' + puuid + '/ids?start=0&count=20' + '&api_key=' + api_key
@@ -77,6 +55,59 @@ def compare_cs(match_history, api_key):
     
     average_difference = total_difference / len(match_history)
     return average_difference
+
+black = (0,0,0)
+white = (255,255,255)
+
+red = (200,0,0)
+green = (0,200,0)
+
+bright_red = (255,0,0)
+bright_green = (0,255,0)
+screen_width = 500
+screen_height = 500
+
+
+def game_start_screen(): #add game start screen
+    game_start = True
+    while game_start: #start screen loop
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT: #if clock exit, quit game
+                    pygame.quit()
+                    quit()
+        window.fill(black)
+        #^ fill in screen black
+
+        
+        #^add image of a chicken
+
+
+        clock = pygame.time.Clock()
+        #^ clock for death screen
+
+        largeText = pygame.font.SysFont("ariel",65)
+        #^ create font to render in text
+
+        #^^ create text boxes for Final Score and Final level for death screen
+        TextSurf, TextRect = text_objects("Welcome to", largeText)
+        LTextSurf, LTextRect = text_objects("Crossy Street!", largeText)
+        #^^ create text boxes for welcoming text
+        TextRect.center = ((screen_width/2),(screen_height/5.5))
+        LTextRect.center = ((screen_width/2),(screen_height/3.5))
+        #^^ set the location of the text boxes
+        window.blit(TextSurf, TextRect)
+        window.blit(LTextSurf, LTextRect)
+        #^ render text onto the window
+
+        button("Quit",250,450,100,50,red,bright_red,quit)
+        #quit and restart buttons^
+        pygame.display.update()
+        clock.tick(15)
+
+window = pygame.display.set_mode((500, 500))
+game_start_screen()
+
+
 
         
 
