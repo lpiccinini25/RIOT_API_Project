2da5ba512609a341027ee17f3ad3f731388a9e91
lpiccinini25 <143562085+lpiccinini25@users.noreply.github.com>
2024-07-18 02:00:55 -0700

    WHY WONT THIS WORK

--- Functions.py
+++ Functions.py
@@ -1,7 +1,10 @@
 import requests
+import pygame
 
-def get_puuid(name, riot_id):
-    puuid_url = 'https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/' + name + '/' + riot_id
+white = (255,255,255)
+
+def get_puuid(name, riot_id, api_key):
+    puuid_url = 'https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/' + name + '/' + riot_id + '?api_key=' + api_key
     resp = requests.get(puuid_url)
     account_info = resp.json()
     puuid = account_info['puuid']
@@ -9,3 +12,4 @@ def get_puuid(name, riot_id):
 
 def get_match_history(name, riot_id):
     'https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/' 
+
