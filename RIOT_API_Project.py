b7a56d005ebea18f48c92f8034a699befca8fd5c
lpiccinini25 <143562085+lpiccinini25@users.noreply.github.com>
2024-07-17 23:26:16 -0700

    adding button function from previous project

--- RIOT_API_Project.py
+++ RIOT_API_Project.py
@@ -2,6 +2,24 @@
 import requests
 import pygame
 
+
+def button(self,msg,x,y,w,h,ic,ac,action=None): #add button function
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(self.window, ac,(x,y,w,h))
+        if click[0] == 1 and action != None:
+            # runs whatever function is input, could be self.run() or quitgame()
+            print(action)
+            action()
+    else: # if no action, meaning button not clicked, then render buttons
+        pygame.draw.rect(self.window, ic,(x,y,w,h))
+        smallText = pygame.font.SysFont("comicsansms",20)
+        textSurf, textRect = text_objects(msg, smallText)
+        textRect.center = ( (x+(w/2)), (y+(h/2)) )
+        self.window.blit(textSurf, textRect)
+
 api_key = 'RGAPI-f2234498-0b43-4612-a09c-afa88c8e7f86'
 api_url = 'https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/llimeincoconut/0000'
 
