b222a898d706c31758e645898b80af27066e7250
lpiccinini25 <143562085+lpiccinini25@users.noreply.github.com>
2024-07-18 01:52:17 -0700

    add import pygame

--- Functions.py
+++ Functions.py
@@ -1,4 +1,5 @@
 import requests
+import pygame
 
 def get_puuid(name, riot_id, api_key):
     puuid_url = 'https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/' + name + '/' + riot_id + '?api_key=' + api_key
