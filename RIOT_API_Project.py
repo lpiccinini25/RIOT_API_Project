7a80d8982d65c49a52bb8dab9f29b8409d584359
lpiccinini25 <143562085+lpiccinini25@users.noreply.github.com>
2024-07-18 02:21:06 -0700

    fixing

--- RIOT_API_Project.py
+++ RIOT_API_Project.py
@@ -105,24 +105,24 @@ def text_objects(text, font): #create text objects
     return textSurface, textSurface.get_rect()
 
 
-def button(self,msg,x,y,w,h,ic,ac,action=None): #add button function
+def button(msg,x,y,w,h,ic,ac,action=None): #add button function
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
 
     if x+w > mouse[0] > x and y+h > mouse[1] > y:
-        pygame.draw.rect(self.window, ac,(x,y,w,h))
+        pygame.draw.rect(window, ac,(x,y,w,h))
         if click[0] == 1 and action != None:
             # runs whatever function is input, could be self.run() or quitgame()
             print(action)
             action()
     else: # if no action, meaning button not clicked, then render buttons
-        pygame.draw.rect(self.window, ic,(x,y,w,h))
+        pygame.draw.rect(window, ic,(x,y,w,h))
         smallText = pygame.font.SysFont("comicsansms",20)
         textSurf, textRect = text_objects(msg, smallText)
         textRect.center = ( (x+(w/2)), (y+(h/2)) )
-        self.window.blit(textSurf, textRect)
+        window.blit(textSurf, textRect)
 
-
+pygame.init()
 window = pygame.display.set_mode((500, 500))
 game_start_screen()
 
